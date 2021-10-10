fefac655163ba49f1b75e455efb017ca0f3f6f64
Hector Siman <hector@verticis.com.br>
2021-10-10 15:19:35 -0300

    update(): makinng connection to the mongo database

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+
 const app = express()
+
+mongoose.connect("mongodb://root:root@mongo:27017/?authSource=admin")
+        .then(() => console.log("successfully connected to DB"))
+        .catch(e => console.log(e))
+
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
